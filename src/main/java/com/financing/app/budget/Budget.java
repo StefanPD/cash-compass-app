bf038cc2ce56644dae31124cb60fe8182205ca0d
stefanPD <stefan.alimpic@productdock.com>
2024-02-02 13:58:51 +0100

    add budget domain models and repository

---
+++ src/main/java/com/financing/app/budget/Budget.java
@@ -0,0 +1,36 @@
+package com.financing.app.budget;
+
+import com.financing.app.user.User;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "budgets")
+@AllArgsConstructor
+@NoArgsConstructor
+public class Budget {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "budget_id")
+    private Long incomeId;
+
+    @Column(precision = 10, scale = 2, name = "total_budget")
+    private BigDecimal totalBudget;
+
+    private int month;
+
+    private int year;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+}
