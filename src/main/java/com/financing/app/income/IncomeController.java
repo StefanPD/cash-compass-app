e3b2fbbd57a458ff7f03d9482028a04ebeaa4d4b
nikolapd89 <nikola.stojanovic@productdock.com>
2024-02-20 15:02:30 +0100

    Introduce income history

--- src/main/java/com/financing/app/income/IncomeController.java
+++ src/main/java/com/financing/app/income/IncomeController.java
@@ -1,10 +1,13 @@
 package com.financing.app.income;
 
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @RestController
@@ -20,5 +23,21 @@ public class IncomeController {
     public ResponseEntity<List<IncomeDTO>> getIncomesByUserId(@PathVariable Long userId) {
         var incomes = incomeService.fetchIncomesByUserId(userId);
         return ResponseEntity.ok(incomes);
+    }
+
+    @GetMapping("incomes/{userId}/history")
+    public ResponseEntity<List<IncomeDTO>> getIncomesByUserId(@PathVariable Long userId,
+                                                              @RequestParam(required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate startDate,
+                                                              @RequestParam(required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate endDate) {
+        if (startDate == null && endDate == null) {
+            endDate = LocalDate.now();
+            startDate = endDate.minusMonths(1);
+        } else if (startDate == null) {
+            startDate = endDate.minusMonths(1);
+        } else if (endDate == null) {
+            endDate = startDate.plusMonths(1);
+        }
+        var history = incomeService.fetchIncomesByHistory(userId, startDate, endDate);
+        return ResponseEntity.ok(history);
     }
 }
--- src/main/java/com/financing/app/income/IncomeRepository.java
+++ src/main/java/com/financing/app/income/IncomeRepository.java
@@ -4,10 +4,13 @@ import com.financing.app.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @Repository
 public interface IncomeRepository extends JpaRepository<Income, Long> {
 
     List<Income> findIncomesByUser(User user);
+
+    List<Income> findIncomeByUserAndIncomeDateBetween(User user, LocalDate startDate, LocalDate endDate);
 }
