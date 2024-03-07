7a42abe2bf67c9993c78bddef5f9e830ef34e365
nikolapd89 <nikola.stojanovic@productdock.com>
2024-03-07 12:50:18 +0100

    Remove redundant properties

--- src/main/java/com/financing/app/AppApplication.java
+++ src/main/java/com/financing/app/AppApplication.java
@@ -9,12 +9,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class AppApplication {
 
-    @Autowired
-    public BudgetRepository budgetRepository;
-
-    @Autowired
-    private BudgetMapper budgetMapper;
-
     public static void main(String[] args) {
         SpringApplication.run(AppApplication.class, args);
     }
