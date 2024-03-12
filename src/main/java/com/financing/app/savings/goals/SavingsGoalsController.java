6b7b61cb5bef1b9e29c5e84557d804ccc5aa66da
stefanPD <stefan.alimpic@productdock.com>
2024-03-12 13:58:44 +0100

    add logging

--- src/main/java/com/financing/app/savings/goals/SavingsGoalsController.java
+++ src/main/java/com/financing/app/savings/goals/SavingsGoalsController.java
@@ -2,6 +2,7 @@ package com.financing.app.savings.goals;
 
 import com.financing.app.utils.ApiVersion;
 import jakarta.validation.constraints.Min;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,6 +14,7 @@ import java.util.List;
 @RestController
 @Validated
 @ApiVersion("api/v1/saving-goal")
+@Slf4j
 public class SavingsGoalsController {
 
     private final SavingsGoalsService savingsGoalsService;
@@ -23,6 +25,7 @@ public class SavingsGoalsController {
 
     @GetMapping("{userId}/savings-goals")
     ResponseEntity<List<SavingsGoalDTO>> getSavingsGoalsByUserId(@PathVariable("userId") @Min(1) Long userId) {
+        log.info("GET request received - api/v1/saving-goal/{userId}/savings-goals, for userId: {}", userId);
         return ResponseEntity.ok(savingsGoalsService.fetchSavingsGoalsByUserId(userId));
     }
 }
